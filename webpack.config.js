1ded1329bad21956ddfe94ac24b95e97280adf6e
natsu-cat <akisancat.natsu@gmail.com>
2020-10-30 16:13:14 +0900

    add webpack.config.js, src\index.tsx, src\templates\index.html, update .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const src = path.resolve(__dirname, 'src')
+const dist = path.resolve(__dirname, 'dist')
+
+const DEV_PORT = process.env.PORT || 3000;
+
+module.exports = {
+    entry: src + '/index.tsx',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: src + '/templates/index.html'
+        }),
+    ],
+    output: {
+        path: dist ,
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts','tsx','js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(dist),
+        port: DEV_PORT,
+        hot: true,
+        historyApiFallback: true,
+    }
+}
