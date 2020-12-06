689d3fc4d60b50029e507b370c2bc3792569c527
natsu-cat <akisancat.natsu@gmail.com>
2020-12-06 22:27:01 +0900

    upgrade Head Home Info Main Side

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,153 +2,186 @@ import * as React from 'react';
 import Table from 'react-bootstrap/Table';
 import Nav from 'react-bootstrap/Nav';
 import Tab from 'react-bootstrap/Tab';
+import { Container } from 'react-bootstrap';
 
-class Foot extends React.Component {
+interface Props {
+    isLoading: Boolean;
+    userDatas: [];
+    flag: number;
+}
 
+class Home extends React.Component<Props, any> {
     render() {
-        return(
-            <div>
-                <Tab.Container defaultActiveKey="link-0">
-                    <Nav variant="pills">
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-0">総合</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-1">1年生</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-2">2年生</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-3">3年生</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-4">4年生</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                   <Tab.Content>
-                       <Tab.Pane eventKey="link-0">
-                            <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th>科目名</th>
-                                        <th>単位</th>
-                                        <th>評価</th>
-                                        <th>年度</th>
-                                        <th>教員名</th>
-                                        <th>学期</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>コミュニケーション技法</td>
-                                        <td>2</td>
-                                        <td>不可</td>
-                                        <td>2020</td>
-                                        <td>Oic 三郎</td>
-                                        <td>後期</td>
-                                    </tr>
-                                    <tr>
-                                        <td>システム開発演習</td>
-                                        <td>7</td>
-                                        <td>優</td>
-                                        <td>2020</td>
-                                        <td>Oic 二郎</td>
-                                        <td>後期</td>
-                                    </tr>
-                                    <tr>
-                                        <td>ゼミナール</td>
-                                        <td>2</td>
-                                        <td>優</td>
-                                        <td>2020</td>
-                                        <td>Oic 太郎</td>
-                                        <td>後期</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Tab.Pane>
-
-                        <Tab.Pane eventKey="link-1">
-                            <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th>科目名</th>
-                                        <th>単位</th>
-                                        <th>評価</th>
-                                        <th>年度</th>
-                                        <th>教員名</th>
-                                        <th>学期</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>コミュニケーション技法</td>
-                                        <td>2</td>
-                                        <td>不可</td>
-                                        <td>2020</td>
-                                        <td>Oic 三郎</td>
-                                        <td>後期</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Tab.Pane>
-
-                        <Tab.Pane eventKey="link-2">
-                            <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th>科目名</th>
-                                        <th>単位</th>
-                                        <th>評価</th>
-                                        <th>年度</th>
-                                        <th>教員名</th>
-                                        <th>学期</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>ゼミナール</td>
-                                        <td>2</td>
-                                        <td>優</td>
-                                        <td>2020</td>
-                                        <td>Oic 太郎</td>
-                                        <td>後期</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Tab.Pane>
-
-                        <Tab.Pane eventKey="link-3">
-                            <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th>科目名</th>
-                                        <th>単位</th>
-                                        <th>評価</th>
-                                        <th>年度</th>
-                                        <th>教員名</th>
-                                        <th>学期</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>システム開発演習</td>
-                                        <td>7</td>
-                                        <td>優</td>
-                                        <td>2020</td>
-                                        <td>Oic 二郎</td>
-                                        <td>後期</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Tab.Pane>
-                        <Tab.Pane eventKey="link-4">
-                        </Tab.Pane>
-                   </Tab.Content>
-                </Tab.Container>
-            </div>
-        );
+        const style: React.CSSProperties = { top: 0, left: 0, width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" };
+        if (this.props.isLoading) {                         //Ajax認証が終わっていない間の処理
+            return (
+                <div style={style}>
+                    <a>Loading...</a>
+                </div>
+            )
+        } else if (this.props.flag == 0) {                  //生徒の場合
+            return (
+                <Container>
+                    <Tab.Container defaultActiveKey="link-0">
+                        <Nav variant="pills">
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-0">総合</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-1">1年生</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-2">2年生</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-3">3年生</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-4">4年生</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                        <Tab.Content>
+                            {showIndivGrade(this.props.userDatas)}
+                        </Tab.Content>
+                    </Tab.Container>
+                </Container>
+            )
+        } else if (this.props.flag == 1) {              //管理者の場合
+            return (
+                <a>管理者</a>
+            )
+        } else{                                         //例外処理
+            return (
+                <a>不正なログイン</a>
+            )
+        }
     }
 }
 
-export default Foot;
+export default Home;
+
+function showIndivGrade(userDatas: any) {
+    const gradeElements: JSX.Element = (
+        <thead>
+            <tr>
+                <th>科目名</th>
+                <th>単位</th>
+                <th>評価</th>
+                <th>学期</th>
+            </tr>
+        </thead>
+    );
+
+    const ADMIS_YEARS = parseInt(userDatas[0].student_number.slice(1, 2), 10);
+    let listAllTables: JSX.Element[] = new Array();
+    let listOneTables: JSX.Element[] = new Array();
+    let listTwoTables: JSX.Element[] = new Array();
+    let listThreeTables: JSX.Element[] = new Array();
+    let listFourTables: JSX.Element[] = new Array();
+
+    for (let i in userDatas) {
+        const SUBJECT_YEARS = parseInt(userDatas[i].Units.slice(3, 4), 10);
+        listAllTables[parseInt(i, 10)] = (
+            <tr key={i}>
+                <td>{userDatas[i].subject_name}</td>
+                <td>{userDatas[i].Dividend_period}</td>
+                <th>{userDatas[i].evaluation}</th>
+                <th>{userDatas[i].Units}</th>
+            </tr>
+        );
+        switch (SUBJECT_YEARS - ADMIS_YEARS) {
+            case 0:
+                listOneTables[parseInt(i, 10)] = (
+                    <tr key={i}>
+                        <td>{userDatas[i].subject_name}</td>
+                        <td>{userDatas[i].Dividend_period}</td>
+                        <th>{userDatas[i].evaluation}</th>
+                        <th>{userDatas[i].Units}</th>
+                    </tr>
+                );
+                break;
+            case 1:
+            case -9:
+                listTwoTables[parseInt(i, 10)] = (
+                    <tr key={i}>
+                        <td>{userDatas[i].subject_name}</td>
+                        <td>{userDatas[i].Dividend_period}</td>
+                        <th>{userDatas[i].evaluation}</th>
+                        <th>{userDatas[i].Units}</th>
+                    </tr>
+                );
+                break;
+            case 2:
+            case -8:
+                listThreeTables[parseInt(i, 10)] = (
+                    <tr key={i}>
+                        <td>{userDatas[i].subject_name}</td>
+                        <td>{userDatas[i].Dividend_period}</td>
+                        <th>{userDatas[i].evaluation}</th>
+                        <th>{userDatas[i].Units}</th>
+                    </tr>
+                );
+                break;
+            case 3:
+            case -7:
+                listFourTables[parseInt(i, 10)] = (
+                    <tr key={i}>
+                        <td>{userDatas[i].subject_name}</td>
+                        <td>{userDatas[i].Dividend_period}</td>
+                        <th>{userDatas[i].evaluation}</th>
+                        <th>{userDatas[i].Units}</th>
+                    </tr>
+                );
+                break;
+            default:
+                break;
+        }
+    }
+    return (
+        <React.Fragment>
+            <Tab.Pane eventKey="link-0">
+                <Table striped bordered hover>
+                    {gradeElements}
+                    <tbody>
+                        {listAllTables}
+                    </tbody>
+                </Table>
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="link-1">
+                <Table striped bordered hover>
+                    {gradeElements}
+                    <tbody>
+                        {listOneTables}
+                    </tbody>
+                </Table>
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="link-2">
+                <Table striped bordered hover>
+                    {gradeElements}
+                    <tbody>
+                        {listTwoTables}
+                    </tbody>
+                </Table>
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="link-3">
+                <Table striped bordered hover>
+                    {gradeElements}
+                    <tbody>
+                        {listThreeTables}
+                    </tbody>
+                </Table>
+            </Tab.Pane>
+            <Tab.Pane eventKey="link-4">
+                <Table striped bordered hover>
+                    {gradeElements}
+                    <tbody>
+                        {listFourTables}
+                    </tbody>
+                </Table>
+            </Tab.Pane>
+        </React.Fragment>
+    )
+}
--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,45 +1,127 @@
 import * as React from 'react';
 import Table from 'react-bootstrap/Table';
 
-class Info extends React.Component {
+interface Props {
+    userDatas: [];
+    isLoading: Boolean;
+    flag: number;
+}
 
+class Info extends React.Component<Props, any> {
     render() {
-        return(
-            <div className="info">
-                <Table striped bordered hover variant="dark">
-                    <thead>
-                        <tr>
-                            <th>学年</th>
-                            <th>進級単位</th>
-                            <th>獲得単位</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>35</td>
-                            <td>40</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>70</td>
-                            <td>78</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>100</td>
-                            <td>105</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>120</td>
-                            <td>105</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
-        );
+        if (this.props.isLoading == false && this.props.flag == 0) {            //読み込み終了かつ生徒の場合のみ獲得単位表示
+            return (
+                <div className="info">
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                                <th>学年</th>
+                                <th>進級単位</th>
+                                <th>獲得単位</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {showCredit(this.props.userDatas)}
+                        </tbody>
+                    </Table>
+                </div>
+            );
+        } else {
+            return renderElseCredit();
+        }
     }
 }
 
 export default Info;
+
+function showCredit(userDatas: any) {
+    const ADMIS_YEARS = parseInt(userDatas[0].student_number.slice(1, 2), 10);
+    let firstCredit: number = 0;
+    let secondCredit: number = 0;
+    let therdCredit: number = 0;
+    let fourthCredit: number = 0;
+    for (let i in userDatas) {
+        const SUBJECT_YEARS = parseInt(userDatas[i].Units.slice(3, 4), 10);
+        switch (SUBJECT_YEARS - ADMIS_YEARS) {
+            case 0:
+                firstCredit += parseInt(userDatas[i].Dividend_period, 10);
+                break;
+            case 1:
+            case -9:
+                secondCredit += parseInt(userDatas[i].Dividend_period, 10);
+                break;
+            case 2:
+            case -8:
+                therdCredit += parseInt(userDatas[i].Dividend_period, 10);
+                break;
+            case 3:
+            case -7:
+                fourthCredit += parseInt(userDatas[i].Dividend_period, 10);
+        }
+    }
+    secondCredit += firstCredit;
+    therdCredit += secondCredit;
+    fourthCredit += therdCredit;
+    return (
+        <React.Fragment>
+            <tr>
+                <td>1</td>
+                <td>35</td>
+                <td>{firstCredit}</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>70</td>
+                <td>{secondCredit}</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>100</td>
+                <td>{therdCredit}</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>120</td>
+                <td>{fourthCredit}</td>
+            </tr>
+        </React.Fragment>
+    );
+}
+
+function renderElseCredit() {
+    return (
+        <div className="info">
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>学年</th>
+                        <th>進級単位</th>
+                        <th>獲得単位</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>35</td>
+                        <td>-</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>70</td>
+                        <td>-</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>100</td>
+                        <td>-</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>120</td>
+                        <td>-</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
+}
