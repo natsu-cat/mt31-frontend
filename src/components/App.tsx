7521cb63cc24c56d901f0d1ea2112d4501e9fd91
natsu-cat <akisancat.natsu@gmail.com>
2020-12-06 22:20:44 +0900

    fix App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 //components
-//import Default from './Default';
 import Login from './Login';
 import Main from './Main';
-class App extends React.Component<any,any> {
-    render() {
-        let isLoggedIn = sessionStorage.getItem("isLoggedIn");              //リダイレクト処理
 
+class App extends React.Component<any, any> {
+    render() {
+        const isLoggedIn = sessionStorage.getItem("isLoggedIn");
         return (
             <BrowserRouter>
-            <Switch>
-                <Route exact path="/login" component={Login} />
-                <Route path="/" render={() => (            //リダイレクト処理
-                     isLoggedIn ? (              
-                         < Main/>
-                     ) : (
-                        <Redirect to="/login" />
-                    )
-                )} />
-            </Switch>
+                <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <Route path="/" render={() => (            //リダイレクト処理
+                        isLoggedIn ? (
+                            < Main />
+                        ) : (
+                                <Redirect to="/login" />
+                            )
+                    )} />
+                </Switch>
             </BrowserRouter>
         );
     }
