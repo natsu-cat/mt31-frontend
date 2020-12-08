7492d4926398f29bbfa56617190c0c37dbc89621
natsu-cat <akisancat.natsu@gmail.com>
2020-12-08 11:22:35 +0900

    fix Home IndivGrade Info Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -24,6 +24,7 @@ class Main extends React.Component<any, any> {
         if (this.state.flag == 0) {                                 //生徒の場合の処理
             getIndivGrade()
                 .then(res => {
+                    console.log(res.data);
                     this.setState({ userDatas: res.data });
                 }).catch(error => {
                     console.error(error);
@@ -55,12 +56,13 @@ class Main extends React.Component<any, any> {
                             <Side flag={this.state.flag} />
                         </Col>
                         <Col lg="7">
-                                <Route exact path="/" render={() => <Home userDatas={this.state.userDatas} isLoading={this.state.isLoading} flag={this.state.flag} />} />
-                                <Route exact path="/upload" render={() => <Upload flag={this.state.flag} />} />
-                                <Route exact path="/registration" render={() => <Registration flag={this.state.flag} />} />
+                            <Route exact path="/" render={() => (
+                                <Home userDatas={this.state.userDatas} isLoading={this.state.isLoading} flag={this.state.flag} username={this.state.username} />)} />
+                            <Route exact path="/upload" render={() => <Upload flag={this.state.flag} />} />
+                            <Route exact path="/registration" render={() => <Registration flag={this.state.flag} />} />
                         </Col>
                         <Col lg="2">
-                            <Info userDatas={this.state.userDatas} isLoading={this.state.isLoading} flag={this.state.flag} />
+                            <Info userDatas={this.state.userDatas} isLoading={this.state.isLoading} flag={this.state.flag} username={this.state.username} />
                         </Col>
                     </Row>
                     <Foot />
