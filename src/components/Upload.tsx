dd8797d147a7c7d27d3b81f065d50e0565b9a3a0
natsu-cat <akisancat.natsu@gmail.com>
2020-12-11 14:41:14 +0900

    fix Upload

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import {Form, Button ,Container, Row, Col} from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import Select from 'react-select'
-import {postGrade,postDepart,postCourse,postStudent,postSubject} from './Auth';
+import { postGrade, postDepart, postCourse, postStudent, postSubject } from './Auth';
 import ReactFileReader from 'react-file-reader';
 
 interface Props {
@@ -10,78 +10,103 @@ interface Props {
 
 class Upload extends React.Component<Props, any> {
 
-    constructor(props: Props){
+    constructor(props: Props) {
         super(props);
-        this.state= {
+        this.state = {
             upload_url: null,
             select_label: 'インポート先が選択されていません',
-            options:[
-                {value:"Course",label:"Course"},
-                {value:"Depart",label:"Depart"},
-                {value:"Grade",label:"Grade"},
-                {value:"Student",label:"Studnet"},
-                {value: "Subject",label:"Subject"}
+            options: [
+                { value: "Course", label: "Course" },
+                { value: "Depart", label: "Depart" },
+                { value: "Grade", label: "Grade" },
+                { value: "Student", label: "Studnet" },
+                { value: "Subject", label: "Subject" }
             ],
+            result: null
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleChangeUrl = this.handleChangeUrl.bind(this);
         this.post_csv = this.post_csv.bind(this);
     }
 
-    handleChange(event: any){
-        this.setState({select_label: event.value});
+    handleChange(event: any) {
+        this.setState({ select_label: event.value });
         this.handleChangeUrl(event.value);
 
     }
 
-    handleChangeUrl(event: any){
-        this.setState({upload_url: event})
+    handleChangeUrl(event: any) {
+        this.setState({ upload_url: event })
     }
 
-    post_csv(files: any){
+    post_csv(files: any) {
 
-        if(this.state.upload_url != null){
+        if (this.state.upload_url != null) {
+            const data = new FormData();
+            data.append("csv", files[0]);
 
-            switch(this.state.upload_url){
+            switch (this.state.upload_url) {
 
                 case "Course":
-                    postCourse(files[0]);
+                    postCourse(data).catch(error => {
+                        console.error(error.response);
+                        this.setState({ result: "エラー： ファイルをインポートできませんでした。" });
+                    });
                     break;
+
                 case "Depart":
-                    postDepart(files[0]);
+                    postDepart(data).catch(error => {
+                        console.error(error.response);
+                        this.setState({ result: "エラー： ファイルをインポートできませんでした。" });
+                    });
                     break;
-    
+
                 case "Grade":
-                    postGrade(files[0]);
+                    postGrade(data).catch(error => {
+                        console.error(error.response);
+                        this.setState({ result: "エラー： ファイルをインポートできませんでした。" });
+                    });
                     break;
-    
+
                 case "Student":
-                    postStudent(files[0]);
+                    postStudent(data).catch(error => {
+                        console.error(error.response);
+                        this.setState({ result: "エラー： ファイルをインポートできませんでした。" });
+                    });
                     break;
+
                 case "Subject":
-                    postSubject(files[0]);
-                    
+                    postSubject(data).catch(error => {
+                        console.error(error.response);
+                        this.setState({ result: "エラー： ファイルをインポートできませんでした。" });
+                    });
+
             }
-            alert("import完了")
+            if (this.state.result) {
+                alert(this.state.result);
+            }
+            else {
+                alert("import完了");
+            }
         }
-        else{
+        else {
             alert("importするファイルの種類を選んでください");
         }
     }
 
     render() {
-        if(this.props.flag != 1) {                      //管理者以外がアクセスした場合の処理
+        if (this.props.flag != 1) {                      //管理者以外がアクセスした場合の処理
             return null;
         }
-        return(
+        return (
             <Container>
                 <Row className="justify-content-center">
                     <Col xs="auto" sm="auto" md="auto" lg="auto" xl="3" >
                         <Form.Label>improtするファイル選択</Form.Label>
-                        <Select isSearchable onChange={ (event) =>this.handleChange(event)} options={this.state.options}/>
+                        <Select isSearchable onChange={(event) => this.handleChange(event)} options={this.state.options} />
                     </Col>
                     <Col xs="auto" sm="auto" md="auto" lg="auto" xl="7" >
-                        <Form  onSubmit={this.post_csv}>
+                        <Form onSubmit={this.post_csv}>
                             <Form.Group>
                                 <Form.Label>{this.state.select_label}</Form.Label>
                                 <ReactFileReader handleFiles={this.post_csv} fileTypes={'.csv'}>
